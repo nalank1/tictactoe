71375c210a892a6475ececfd6d4cc98bca86360a
Nalan Kurnaz <nalan.kurnaz99@gmail.com>
2024-06-19 12:11:05 -0400

    Finished Version of the Game

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 let cells = document.querySelectorAll(".cellButton");
 let resetButton = document.querySelector("#resetBtn");
+let newGateBtn = document.querySelector("#playAgainBtn");
+let winnerText = document.querySelector("#winnerText");
+let winnercontainer = document.querySelector(".modalContent");
 
 let turnO = true; //playerX, playerY, to interchange between players
 
@@ -15,7 +18,7 @@ const winning_conditions = [
 ]; //better way to organise for using 2d array
 
 cells.forEach((cellButton) => {
-  cellButton.addEventListener("click", (cellClicked) => {
+  cellButton.addEventListener("click", () => {
     console.log("The box was clicked");
     if (turnO) {
       cellButton.innerHTML = "O";
@@ -25,5 +28,72 @@ cells.forEach((cellButton) => {
       turnO = true;
     }
     cellButton.disabled = true;
+
+    checkWinner();
   });
+});
+
+const showWinner = (winner) => {
+  winnerText.innerText = `The winner is ${winner}`;
+  winnercontainer.classList.remove("hide");
+  disableCells();
+};
+
+const disableCells = () => {
+  for (let cell of cells) {
+    cell.disabled = true;
+  }
+};
+
+const enableCells = () => {
+  for (let cell of cells) {
+    cell.disabled = false;
+    cell.innerText = "";
+  }
+};
+
+const checkWinner = () => {
+  let draw = true;
+  for (let pattern of winning_conditions) {
+    console.log(pattern[0], pattern[1], pattern[2]);
+
+    let pos1 = cells[pattern[0]].innerText;
+    let pos2 = cells[pattern[1]].innerText;
+    let pos3 = cells[pattern[2]].innerText;
+
+    if (pos1 != "" && pos2 != "" && pos3 != "") {
+      if (pos1 === pos2 && pos2 === pos3) {
+        console.log("winner", pos1);
+        showWinner(pos1);
+        return;
+      }
+    } else {
+      draw = false;
+    }
+  }
+
+  if (draw) {
+    console.log("draw");
+    showDraw();
+  }
+};
+
+const showDraw = () => {
+  winnerText.innerText = `It's a draw`;
+  winnercontainer.classList.remove("hide");
+  disableCells();
+};
+
+const resetGame = () => {
+  turnO = true;
+  enableCells();
+  winnercontainer.classList.add("hide");
+};
+
+playAgainBtn.addEventListener("click", () => {
+  resetGame();
+});
+
+resetButton.addEventListener("click", () => {
+  resetGame();
 });
